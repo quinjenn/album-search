e8847960d7be5d39d12576eb662547419fddfc0c
quinjenn <jquintal_@hotmail.com>
2023-04-04 17:49:59 -0700

    git motified LiveSearch to remove hard corded term, final code

--- react-albums/src/components/LiveSearch.js
+++ react-albums/src/components/LiveSearch.js
@@ -1,11 +1,42 @@
-import React, { Fragment, useState } from "react";
+// import React, { Fragment, useState, useEffect } from "react";
+// import axios from "axios";
+// import SearchBar from "components/SearchBar";
+// import Results from "components/Results";
 
+// export default function LiveSearch(props) {
+//   const [term, setTerm] = useState("");
+//   const [results, setResults] = useState([]);
+
+//   useEffect(() => {
+//     const baseURL = `https://itunes.apple.com/search?country=CA&media=music&entity=album&attribute=artistTerm`;
+//     const searchURL = term ? `${baseURL}&term=${term}` : `${baseURL}&term=beatles`;
+
+//     axios.get(searchURL).then(response => {
+//       console.log(response.data.results);
+//       // setResults(response.data.results);
+//       setResults([...response.data.results])
+//     });
+//   }, [term]);
+
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
 import SearchBar from "components/SearchBar";
 import Results from "components/Results";
 
 export default function LiveSearch(props) {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const baseURL = `https://itunes.apple.com/search?country=CA&media=music&entity=album&attribute=artistTerm`;
+    const searchURL = term ? `${baseURL}&term=${term}` : `${baseURL}&term=${term}`;
+
+    axios.get(searchURL).then(response => {
+      console.log(response.data.results);
+      // setResults(response.data.results);
+      setResults([...response.data.results]);
+    });
+  }, [term]);
 
   return (
     <Fragment>
